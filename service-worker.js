06c994a8f55ff72274b4fd6265f5d6473a8ebb3b
Siyu Long <i@longern.com>
2022-07-19 16:53:21 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('paymentrequest', async (event) => {
   const promise = new Promise(() => {});
   event.respondWith(promise);
-  const redirect_url = event.methodData[0].data.redirect;
+  const redirect_url = (event.methodData[0].data || {}).redirect_url;
   const params = new URLSearchParams();
   if (redirect_url) params.set("redirect", redirect_url);
   const win = await event.openWindow(`/frontend?${params}`);
